1c3bfb413e8bbd9d37a18536c40386957aa639da
hassaammgl <h455a4mmehtab@gmail.com>
2025-07-21 01:04:41 +0500

    feat: enhance Home component with GSAP animations and structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -8,6 +9,8 @@ const App = () => {
   return (
     <main>
       <Navbar />
+      <Home />
+      <div className="h-dvh bg-black" />
     </main>
   )
 }
