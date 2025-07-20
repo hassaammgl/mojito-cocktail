54bf7908334e9271f4de6390febbb79151814d27
hassaammgl <h455a4mmehtab@gmail.com>
2025-07-21 01:16:07 +0500

    feat: add responsive video handling and improve GSAP animations in Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
 import gsap from "gsap"
 import { useGSAP } from "@gsap/react"
 import { SplitText } from "gsap/all"
-
+import { useRef } from "react"
+import { useMediaQuery } from "react-responsive"
 
 const Home = () => {
+
+    const videoRef = useRef();
+    const isMobile = useMediaQuery();
 
     useGSAP(() => {
         const heroSplit = new SplitText(".title", { type: 'chars, words' });
@@ -37,6 +41,8 @@ const Home = () => {
             .to('.right-leaf', { y: 200 }, 0)
             .to('.left-leaf', { y: -200 }, 0)
 
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
     }, [])
 
     return (
@@ -72,6 +78,15 @@ const Home = () => {
                     </div>
                 </div>
             </section>
+            <div className="video absolute inset-0">
+                <video
+                    ref={videoRef}
+                    src="/video/input.mp4"
+                    muted
+                    playsInline
+                    preload="auto"
+                />
+            </div>
         </>
     )
 }
