fec50bdf9369a49f263ab692492165a16a0b3744
hassaammgl <h455a4mmehtab@gmail.com>
2025-07-22 01:31:23 +0500

    feat: enhance About component with GSAP animations for title and grid elements

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,38 @@
+import { useGSAP } from "@gsap/react"
+import { SplitText } from "gsap/all"
+import gsap from "gsap"
 
 const About = () => {
+
+    useGSAP(() => {
+        const titleSplit = SplitText.create('#about h2', {
+            type: 'words'
+        });
+
+        const scrollTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#about',
+                start: 'top center'
+            }
+        })
+
+        scrollTimeline
+            .from(titleSplit.words, {
+                opacity: 0,
+                duration: 1,
+                yPercent: 100,
+                ease: 'expo.out',
+                stagger: .02
+            })
+            .from('.top-grid div, .bottom-grid div', {
+                opacity: 0,
+                duration: 1,
+                ease: 'power1.inOut',
+                stagger: 0.04,
+            }, '-=0.5')
+
+    })
+
     return (
         <section id="about">
             <div className="mb-16 md:px-0 px-5">
