50d14154166cf4065d97b4ba8afd382816997bf6
hassaammgl <h455a4mmehtab@gmail.com>
2025-07-22 02:05:52 +0500

    feat: add Art component with GSAP animations and integrate into App

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
+import Art from "./components/Art";
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -14,6 +15,7 @@ const App = () => {
       <Home />
       <Cocktails />
       <About />
+      <Art />
     </main>
   )
 }
